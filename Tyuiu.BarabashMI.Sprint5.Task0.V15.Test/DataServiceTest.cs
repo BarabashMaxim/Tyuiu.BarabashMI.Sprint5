16ce17feecc7ff494f3636bffdd1b55f2ef9547c
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-11-23 14:42:49 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.BarabashMI.Sprint5.Task0.V15.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Tyuiu.BarabashMI.Sprint5.Task0.V15.Lib;
+namespace Tyuiu.BarabashMI.Sprint5.Task0.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = $@"E:\.solution\Tyuiu.BarabashMI.Sprint5\Tyuiu.BarabashMI.Sprint5.Task0.V15.Lib\bin\Debug\net8.0\OutPutFileTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.BarabashMI.Sprint5.Task0.V15/Program.cs
@@ -0,0 +1,4 @@
+﻿using Tyuiu.BarabashMI.Sprint5.Task0.V15.Lib;
+DataService ds = new DataService();
+string res = ds.SaveToFileTextData(3);
+Console.WriteLine(res);
