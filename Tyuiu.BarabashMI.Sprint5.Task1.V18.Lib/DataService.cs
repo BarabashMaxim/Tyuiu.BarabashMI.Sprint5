59523a4738aba57861523bb5313edc55d74975ff
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-11-23 16:21:12 +0500

    Task1

---
+++ Tyuiu.BarabashMI.Sprint5.Task1.V18.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.BarabashMI.Sprint5.Task1.V18.Lib
+{
+    public class DataService : ISprint5Task1V18
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+
+            double y;
+            string stry;
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+
+            if (fileExist)
+            { 
+                File.Delete(path);
+            }
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = 3 * x + 2 - (2 * x - x) / (Math.Cos(x) + 1);
+                stry = Convert.ToString(y);
+
+                if (x != stopValue)
+                    File.AppendAllText(path, stry + Environment.NewLine);
+                else
+                    File.AppendAllText(path, stry);
+
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.BarabashMI.Sprint5.Task1.V18.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BarabashMI.Sprint5.Task1.V18.Lib;
+namespace Tyuiu.BarabashMI.Sprint5.Task1.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int start = -5;
+            int end = 5;
+            string path = ds.SaveToFileTextData(start, end);
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            Assert.IsTrue(fileExist);
+        }
+    }
+}
