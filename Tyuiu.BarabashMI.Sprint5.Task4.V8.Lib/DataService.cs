8f845de8af40ba50db6e98c9984f74e0ddd0b5b5
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-11-25 13:06:08 +0500

    Task4

---
+++ Tyuiu.BarabashMI.Sprint5.Task4.V8.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.BarabashMI.Sprint5.Task4.V8.Lib
+{
+    public class DataService : ISprint5Task4V8
+    {
+        public double LoadFromDataFile(string path)
+        {
+            NumberFormatInfo info = new NumberFormatInfo
+            {
+                NumberDecimalSeparator = "."
+            };
+            string strX = File.ReadAllText(path);
+            double x = double.Parse(strX, info);
+            double res = Math.Pow(x, 2) / Math.Sin(x) + 3;
+            return Math.Pow(res,3);
+        }
+    }
+}
---
+++ Tyuiu.BarabashMI.Sprint5.Task4.V8.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.BarabashMI.Sprint5.Task4.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
