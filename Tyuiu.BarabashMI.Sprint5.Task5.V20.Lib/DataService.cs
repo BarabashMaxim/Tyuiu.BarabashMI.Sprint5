321959c60ed22eee49e3b6a036e04fa42db83ddb
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-11-25 13:40:51 +0500

    Task5

---
+++ Tyuiu.BarabashMI.Sprint5.Task5.V20.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.BarabashMI.Sprint5.Task5.V20.Lib
+{
+    public class DataService : ISprint5Task5V20
+    {
+        public double LoadFromDataFile(string path)
+        {
+            NumberFormatInfo info = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = "."
+            };
+            string strX = File.ReadAllText(path);
+            string[] strings = strX.Split(",");
+            List<int> numbers = new List<int>();
+            foreach (string str in strings)
+            {
+                if (int.TryParse(str.Trim(), out int number) & number>=-10 & number <=10)
+                    numbers.Add(number);
+            }
+            double res = 0;
+            foreach (int number in numbers)
+            { 
+                res += number;
+            }
+            res /= numbers.Count;
+            return res;
+        }
+    }
+}
