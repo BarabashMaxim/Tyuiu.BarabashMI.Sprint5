c45dccea375cc1c810291cf33d6b9ae90e6d71be
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-11-25 14:30:45 +0500

    Task7

---
+++ Tyuiu.BarabashMI.Sprint5.Task7.V1.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.BarabashMI.Sprint5.Task7.V1.Lib
+{
+    public class DataService : ISprint5Task7V1
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string strX = File.ReadAllText(path);
+            foreach (char c in strX)
+            { 
+                if (char.IsDigit(c))
+                    strX = strX.Remove(c);
+            }
+            return strX;
+            
+        }
+
+    }
+}
