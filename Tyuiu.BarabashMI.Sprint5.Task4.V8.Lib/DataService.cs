950312831a8ae7c0d584a2cd1f269f3d629f84b8
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-11-25 13:07:18 +0500

    T4P1

--- Tyuiu.BarabashMI.Sprint5.Task4.V8.Lib/DataService.cs
+++ Tyuiu.BarabashMI.Sprint5.Task4.V8.Lib/DataService.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.BarabashMI.Sprint5.Task4.V8.Lib
             string strX = File.ReadAllText(path);
             double x = double.Parse(strX, info);
             double res = Math.Pow(x, 2) / Math.Sin(x) + 3;
-            return Math.Pow(res,3);
+            return Math.Round(res,3);
         }
     }
 }
