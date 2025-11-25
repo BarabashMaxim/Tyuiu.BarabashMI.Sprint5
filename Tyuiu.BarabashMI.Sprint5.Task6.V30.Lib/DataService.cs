f8fe335e321e6db57c12d9e9a1abde966ce9d8b1
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-11-25 14:17:13 +0500

    Task6

---
+++ Tyuiu.BarabashMI.Sprint5.Task6.V30.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.BarabashMI.Sprint5.Task6.V30.Lib
+{
+    public class Class1 : ISprint5Task6V30
+    {
+        public int LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            string[] words = strX.Split(" ");
+            int count = 0;
+            foreach (string str in words)
+            { 
+                if (str.Length == 8)
+                {
+                    count += 1;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.BarabashMI.Sprint5.Task6.V30.Test/DataService.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.BarabashMI.Sprint5.Task6.V30.Test
+{
+    [TestClass]
+    public sealed class DataService
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
